1f160a516b4ca1549b7df54d308f3cad197f04b4
sluongo718 <sluongo718@yahoo.com>
2021-07-07 01:42:32 -0400

    commit tree 85a19b36ea07053adf25da1d3f9f6e1f6aa81a64

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function mapToNegativize(arr) {
+    return arr.map(x => x * -1);
+}
+
+function mapToNoChange(arr) {
+   return arr
+}
+
+function mapToDouble(arr) {
+    return arr.map(x => x * 2);
+}
+
+function mapToSquare(arr) {
+    return arr.map(x => x * x);
+}
+
+const reduceToTotal = (arr, startingPoint = 0) => {
+    return arr.reduce((sum, current) => sum + current, startingPoint)
+}
+
+const reduceToAllTrue = (arr) => {
+    return arr.every(x => x)
+}
+
+const reduceToAnyTrue = (arr) => {
+    let curr = false
+    arr.forEach(e => {
+        if (e === true) curr = true
+    })
+    return curr
+}
